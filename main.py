b84587b435aaa15da4ea9482aa209354d0724e7f
Kevin O <85694323+Kevin-Ravasco2@users.noreply.github.com>
2022-06-19 00:15:24 +0300

    add get and post books and authors endpoint

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from fastapi_sqlalchemy import db
 
-app = FastAPI()
+from settings import app
+from models import Book, Author
+from schema import BookSchema, AuthorSchema
 
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello World'}
+    return {'message': 'Hello JOmba'}
+
+
+@app.post('/create-book/', response_model=BookSchema)
+async def create_book(book: BookSchema):
+    book = Book(**book.dict())
+    db.session.add(book)
+    db.session.commit()
+    return book
+
+
+@app.post('/create-author/', response_model=AuthorSchema)
+async def create_author(author: AuthorSchema):
+    author = Author(**author.dict())
+    db.session.add(author)
+    db.session.commit()
+    return author
+
+
+@app.get('/books/')
+async def get_books():
+    return db.session.query(Book).all()
+
+
+@app.get('/authors/')
+async def get_authors():
+    return db.session.query(Author).all()
