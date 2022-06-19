b95753ccf58b727d9a692bbc4ee47e5891360965
Kevin O <85694323+Kevin-Ravasco2@users.noreply.github.com>
2022-06-19 17:45:36 +0300

    Add status 201 code to create

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi import status, HTTPException, Response
 from fastapi_sqlalchemy import db
 
 from settings import app
@@ -6,31 +7,51 @@ from schema import BookSchema, AuthorSchema
 
 
 @app.get('/')
-async def root():
-    return {'message': 'Hello JOmba'}
+async def get_books():
+    """Returns all the books available"""
+    return db.session.query(Book).all()
 
 
-@app.post('/create-book/', response_model=BookSchema)
+@app.get('/authors/', )
+async def get_authors():
+    """Returns all the authors"""
+    return db.session.query(Author).all()
+
+
+@app.post('/create-book/', response_model=BookSchema, status_code=status.HTTP_201_CREATED)
 async def create_book(book: BookSchema):
+    """Creates a new book"""
+
     book = Book(**book.dict())
     db.session.add(book)
     db.session.commit()
     return book
 
 
-@app.post('/create-author/', response_model=AuthorSchema)
+@app.post('/create-author/', response_model=AuthorSchema, status_code=status.HTTP_201_CREATED)
 async def create_author(author: AuthorSchema):
+    """Creates a new author"""
+
     author = Author(**author.dict())
     db.session.add(author)
     db.session.commit()
     return author
 
 
-@app.get('/books/')
-async def get_books():
-    return db.session.query(Book).all()
+@app.get('/book/{book_id}/', response_model=BookSchema)
+async def get_book(book_id: int):
+    """Returns a book by id"""
+    book = db.session.query(Book).get(book_id)
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    return book
 
 
-@app.get('/authors/')
-async def get_authors():
-    return db.session.query(Author).all()
+@app.get('/author/{author_id}/', response_model=AuthorSchema)
+async def get_author(author_id: int):
+    """Returns an author by id"""
+    author = db.session.query(Author).get(author_id)
+    if not author:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Author not found")
+    return author
+
--- schema.py
+++ schema.py
@@ -1,3 +1,6 @@
+import datetime
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr
 
 
@@ -5,6 +8,8 @@ class AuthorSchema(BaseModel):
     name: str
     email: EmailStr
     bio: str
+    date_created: Optional[datetime.datetime]
+    date_modified: Optional[datetime.datetime]
 
     class Config:
         orm_mode = True
